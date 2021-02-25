118da9efe5b790885d662a45775565b89deff8fd
AnhelinaVr <gelina242@gmail.com>
2021-02-25 14:50:47 +0300

    feat: add basic funtionality and styles

---
+++ src/components/App/index.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./App.scss";
+import NumberDisplay from "../NumberDisplay";
+import { generateCells } from "../../utils";
+import Button from "../Button";
+
+const App: React.FC = () => {
+  const [cells, setCells] = useState(generateCells());
+
+  console.log("cells", cells);
+
+  const renderCells = (): React.ReactNode => {
+    return cells.map((row, rowIndex) =>
+      row.map((cell, colIndex) => (
+        <Button
+          key={`${rowIndex}-${colIndex}`}
+          row={rowIndex}
+          col={colIndex}
+          state={cell.state}
+          value={cell.value}
+        />
+      ))
+    );
+  };
+
+  return (
+    <div className="App">
+      <div className="Header">
+        <NumberDisplay value={0} />
+        <div className="Face">
+          <span role="img" aria-label="face">
+            😊
+          </span>
+        </div>
+        <NumberDisplay value={23} />
+      </div>
+      <div className="Body">{renderCells()}</div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Button.scss";
+import { CellState, CellValue } from "../../types";
+
+interface ButtonProps {
+  row: number;
+  col: number;
+  state: CellState;
+  value: CellValue;
+}
+
+const Button: React.FC<ButtonProps> = ({ row, col, state, value }) => {
+  const renderContent = (): React.ReactNode => {
+    if (state === CellState.Visible) {
+      if (value === CellValue.Bomb)
+        return (
+          <span role="img" aria-label="bomb">
+            💣
+          </span>
+        );
+      else if (value === CellValue.None) {
+        return null;
+      }
+      return value;
+    } else if (state === CellState.Flagged) {
+      return (
+        <span role="img" aria-label="flag">
+          🚩
+        </span>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <div
+      className={`Button ${
+        state === CellState.Visible ? "visible" : ""
+      } value-${value}`}
+    >
+      {renderContent()}
+    </div>
+  );
+};
+
+export default Button;
